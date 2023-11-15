0033513c7a5b296f9e69a0300059c2383a8d8d51
Zoee Silcock <mrzoee@gmail.com>
2023-11-15 18:31:50 +0100

    Move Element state to a resource

--- Element/Element.gd
+++ Element/Element.gd
@@ -8,9 +8,7 @@ extends Node2D
 
 # Settings
 @export var element_index : int
-@export var slice_count : int
-@export var initial_radius : float
-@export var shape_rotation : float
+@export var state : ElementState
 
 # Internal
 var slices
@@ -30,16 +28,16 @@ func _on_slice_selected(slice_index : int):
 
 func _instantiate_slices():
 	slices = []
-	slices.resize(slice_count)
+	slices.resize(state.slice_count)
 
-	for i in slice_count:
+	for i in state.slice_count:
 		slices[i] = shape_scene.instantiate()
 		slices_node.add_child(slices[i])
 
 		slices[i].slice_index = i
 		slices[i].element_index = element_index
 		slices[i].name = "Slice" + str(i)
-		slices[i].position = Vector2(initial_radius, 0)
+		slices[i].position = Vector2(state.radius, 0)
 
 		slices[i].position_changed.connect(_on_position_changed)
 		slices[i].selected.connect(_on_slice_selected)
@@ -48,12 +46,16 @@ func _update_slices(slice_index : int = 0):
 	var slice = slices[slice_index]
 	var origin_radius = sqrt(pow(slice.position.x, 2) + pow(slice.position.y, 2))
 	var origin_theta = atan2(slice.position.y, slice.position.x)
-	var theta_increment = deg_to_rad(360.0 / slice_count)
+	var theta_increment = deg_to_rad(360.0 / state.slice_count)
 
-	for i in slice_count:
+	for i in state.slice_count:
 		var theta = (slice_index - i) * theta_increment
 		var slice_position = (origin_radius * Vector2.from_angle(theta + origin_theta))
 
 		if (i != slice_index):
 			slices[i].position = slice_position - position
-		slices[i].rotation = origin_theta + deg_to_rad(shape_rotation) + theta
+		slices[i].rotation = origin_theta + deg_to_rad(state.slice_rotation) + theta
+
+	# Update the state
+	state.radius = origin_radius
+	state.slice_rotation = slices[0].rotation
---
+++ Element/ElementState.gd
@@ -0,0 +1,7 @@
+class_name ElementState
+extends Resource
+
+# Data
+@export var slice_count : int
+@export var radius : float
+@export var slice_rotation : float
