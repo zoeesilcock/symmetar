a96d840622a3b3c29bdbea07656338d59caee861
Zoee Silcock <mrzoee@gmail.com>
2023-11-19 09:55:34 +0100

    Fix a couple of gdscript warnings

--- Document/DocumentFormatLoader.gd
+++ Document/DocumentFormatLoader.gd
@@ -4,7 +4,7 @@ extends ResourceFormatLoader
 func _get_recognized_extensions() -> PackedStringArray:
 	return PackedStringArray(["smtr"])
 
-func _load(path: String, _original_path: String, use_sub_threads : bool, cache_mode : int) -> Variant:
+func _load(path: String, _original_path: String, _use_sub_threads : bool, _cache_mode : int) -> Variant:
 	var file = FileAccess.open(path, FileAccess.READ)
 	var resource = str_to_var(file.get_pascal_string())
 	file.close()
