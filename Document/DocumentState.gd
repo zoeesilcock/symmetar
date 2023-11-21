4ce3f9269ab011b6ac7f6b14295b15af7a1816bc
Zoee Silcock <mrzoee@gmail.com>
2023-11-21 08:43:04 +0100

    Refactor undo logic so that each part of the code is in a more suitable class

--- Document/DocumentState.gd
+++ Document/DocumentState.gd
@@ -11,3 +11,40 @@ extends Resource
 
 # Signals
 signal elements_count_changed
+
+func calculate_diff() -> DocumentStateDiff:
+	var diff : Array[ElementState] = []
+	var count_change = len(elements) - len(previous_elements)
+	var element_properties = ElementState.get_exported_properties()
+
+	for index in len(elements):
+		var element = elements[index]
+		var previous_element = ElementState.new()
+
+		if index < len(previous_elements):
+			previous_element = previous_elements[index]
+
+		diff.push_back(element.get_diff(previous_element))
+
+	return DocumentStateDiff.new(count_change, diff)
+
+func apply_diff(diff : DocumentStateDiff, reverse : bool) -> void:
+	var current_count = len(elements)
+	var direction = 1
+	var element_properties = ElementState.get_exported_properties()
+	if reverse: direction = -1
+
+	if (diff.element_count_change != 0):
+		elements.resize(current_count + diff.element_count_change * direction)
+		_fill_empty_slots()
+
+	for index in len(diff.element_changes):
+		if index < len(elements):
+			elements[index].apply_diff(diff.element_changes[index], direction)
+
+	elements_count_changed.emit(diff.element_count_change * direction)
+
+func _fill_empty_slots() -> void:
+	for i in len(elements):
+		if elements[i] == null:
+			elements[i] = ElementState.new()
--- Element/ElementState.gd
+++ Element/ElementState.gd
@@ -15,3 +15,25 @@ extends Resource
 
 # Signals
 signal slice_count_changed
+
+static func get_exported_properties() -> Array[String]:
+	var exported_properties : Array[String] = []
+	for property in ElementState.new().get_property_list():
+		if property["usage"] == PROPERTY_USAGE_DEFAULT + PROPERTY_USAGE_SCRIPT_VARIABLE :
+			exported_properties.push_back(property["name"])
+
+	return exported_properties
+
+func get_diff(other_element : ElementState) -> ElementState:
+	var element_properties = ElementState.get_exported_properties()
+	var element_diff = ElementState.new()
+
+	for property in element_properties:
+		element_diff[property] = self[property] - other_element[property]
+
+	return element_diff
+
+func apply_diff(element_diff : ElementState, direction : int) -> void:
+	var element_properties = ElementState.get_exported_properties()
+	for property in element_properties:
+		self[property] += element_diff[property] * direction
