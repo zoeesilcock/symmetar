4d6ce3fe885585c523ddc724ffffebcfc6ebd7c3
Zoee Silcock <mrzoee@gmail.com>
2024-01-10 17:21:04 +0100

    Center window on screen

--- Main.gd
+++ Main.gd
@@ -7,8 +7,9 @@ extends Node2D
 func _ready() -> void:
 	ui_state.main_window_scale = _calculate_main_scale()
 
-	_set_main_window_scale()
 	_set_main_window_size()
+	_set_main_window_scale()
+	_set_main_window_position()
 
 func _set_main_window_size() -> void:
 	var default_window_size : Vector2 = Vector2(
@@ -21,6 +22,12 @@ func _set_main_window_size() -> void:
 
 func _set_main_window_scale() -> void:
 	get_tree().root.content_scale_factor = ui_state.main_window_scale
+
+func _set_main_window_position() -> void:
+	var screen_center : Vector2 = DisplayServer.screen_get_size() / 2
+	var window_size : Vector2 = DisplayServer.window_get_size()
+
+	DisplayServer.window_set_position(screen_center - window_size / 2)
 
 func _calculate_main_scale() -> float:
 	var main_scale : float = 1.0
