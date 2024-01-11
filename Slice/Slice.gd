3fe3236c6f8a99b40a950a8794af92cfd957867e
Zoee Silcock <mrzoee@gmail.com>
2024-01-11 08:34:12 +0100

    Fix name of private method

--- Slice/Slice.gd
+++ Slice/Slice.gd
@@ -336,19 +336,19 @@ func _on_selection_changed() -> void:
 		ui_state.selected_slice_index == slice_index
 	)
 
-func get_transformed_polygon() -> PackedVector2Array:
+func _get_transformed_polygon() -> PackedVector2Array:
 	var transformed_polygon : PackedVector2Array = []
 	var translation : Transform2D = Transform2D(rotation, polygon.scale, 0, slice_pivot.rotated(rotation))
 
-	for _vector : Vector2 in polygon.polygon:
-		transformed_polygon.append(translation * _vector)
+	for point : Vector2 in polygon.polygon:
+		transformed_polygon.append(translation * point)
 
 	return transformed_polygon
 
 func _is_point_in_slice(point : Vector2) -> bool:
 	return Geometry2D.is_point_in_polygon(
 		point,
-		get_transformed_polygon()
+		_get_transformed_polygon()
 	)
 
 # Calculate the extents of the shape.
