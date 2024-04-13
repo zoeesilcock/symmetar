c8a93d9d219ccf3482153f3b05a8a8c1845f01ed
Zoee Silcock <mrzoee@gmail.com>
2024-04-13 10:56:49 +0200

    Add a way to quickly see the diffs applied by a DocumentStateDiff

--- Document/DocumentStateDiff.gd
+++ Document/DocumentStateDiff.gd
@@ -5,6 +5,17 @@ extends Resource
 @export var element_diffs: Array[ElementStateDiff]
 @export var background_color_change: Color
 
+@export_multiline var diff_preview: String:
+	get:
+		var preview: String = "Diffs:\n"
+
+		for diff:ElementStateDiff in element_diffs:
+			preview += str(diff.index) + ": "
+			for change: ElementPropertyDiff in diff.changes:
+				preview += "  * " + change.property + " = " + str(change.change) + "\n"
+
+		return preview
+
 func _init(
 		p_element_count_change: int=0,
 		p_element_diffs: Array[ElementStateDiff]=[],
